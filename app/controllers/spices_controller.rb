9d32b5f2c7a83dd8df0d4d75db18a0eec7c3e6d7
raquel <raquelanaroman@gmail.com>
2021-08-17 15:12:17 -0400

    commit tree 96fc5597ebf8b9c08b4cf30abc3bd90ba366e7dd

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,45 @@
+class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_resp_not_found
+
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def show
+        spice = find_spice
+        render json: spice
+    end
+    
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice        
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def render_resp_not_found
+        render json: {error: "Spice not found"}, status: :not_found        
+    end
+
+    def find_spice
+        Spice.find(params[:id])
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end     
+
+end
